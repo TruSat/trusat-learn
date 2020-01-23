b5ef9c3ea63ef252b13361535c6ab837a779a5d6
John Gribbin <johnggribbin@gmail.com>
2020-01-23 17:02:22 -0500

    Update projectName

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -31,7 +31,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: "TruSat Learn",
+  projectName: "trusat-learn",
   organizationName: "TruSat",
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
