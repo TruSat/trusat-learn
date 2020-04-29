766b76b75cf952f1084b9fb4d4aa6ae73274a23f
Michael Deal <michael.deal@consensys.net>
2020-04-29 17:35:59 -0400

    Remove copyright

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -75,7 +75,7 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} ConsenSys Space`,
+  copyright: '',
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
